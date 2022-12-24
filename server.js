144350213fe792827d61aee169a6a8e909dddc2e
Son Le Tran <letranson1992@gmail.com>
2022-12-24 15:43:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server-auth')
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
